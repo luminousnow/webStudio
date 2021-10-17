43dba4ed272b1cec81c56f8ff0fde7f905aed9b1
Denis <denis.kravchuk.ua@gmail.com>
2021-10-17 11:45:56 +0300

    addeed full filtration code

--- js/filter.js
+++ js/filter.js
@@ -1,8 +1,30 @@
 const filter = document.querySelector(".filter");
-const portfolioList = document.querySelector(".portfolio__list");
+const portfolioList = document.querySelectorAll(".portfolio__list .card");
 
-filter.addEventListener("click", (e) => console.log(e.target));
+filter.addEventListener("click", filtration);
 
-function getFilterButton(e) {
-  if(e.target.closest(".filter__item"))
+function filtration(e) {
+  const isButonPressed = e.target.closest(".filter__item");
+
+  if (isButonPressed) {
+    const filterButtonPressed = e.target.textContent;
+    const filterResetButtonPressed =
+      document.querySelector(".filter__button").textContent;
+
+    portfolioList.forEach((el) => {
+      const kindeText = el.querySelector(".card__kinde").textContent;
+      const portfolioElementStyle = el.classList;
+
+      if (
+        filterButtonPressed === filterResetButtonPressed ||
+        filterButtonPressed === kindeText
+      ) {
+        portfolioElementStyle.remove("hidden");
+        console.log(portfolioElementStyle);
+      } else {
+        portfolioElementStyle.add("hidden");
+        console.log(portfolioElementStyle);
+      }
+    });
+  }
 }
